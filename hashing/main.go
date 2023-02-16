33dbee6bf83f92c4c42c6ff28add02a50c793337
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2023-02-16 12:22:58 -0300

    feat: data from flags

--- hashing/main.go
+++ hashing/main.go
@@ -4,12 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"github.com/lripardo/lrw"
+	"os"
 )
 
 func main() {
-	data := flag.Arg(0)
+	var data string
+	flag.StringVar(&data, "d", "changeme", "Data for hashing. (Default - changeme).")
+	flag.Parse()
 
 	config := lrw.DefaultStartServiceParams()
-	fmt.Println(lrw.HashSHA512(data))
-	fmt.Println(lrw.HashPassword(data, config.BCryptCost))
+	fmt.Println("Hashing data from: " + data)
+	fmt.Println("HashSHA512: " + lrw.HashSHA512(data))
+	pass, err := lrw.HashPassword(data, config.BCryptCost)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("BCryptPass: " + pass)
 }
