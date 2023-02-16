34c9fbcad8b2acc902dcab32115a41eb59a44488
Luiz Ricardo Ripardo <ricardo_ripardo@hotmail.com>
2023-02-16 12:11:48 -0300

    feat: hash program from line command

--- hashing/main.go
+++ hashing/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/lripardo/lrw"
 )
 
 func main() {
+	data := flag.Arg(0)
+
 	config := lrw.DefaultStartServiceParams()
-	fmt.Println(lrw.HashSHA512("changeme"))
-	fmt.Println(lrw.HashPassword("changeme", config.BCryptCost))
+	fmt.Println(lrw.HashSHA512(data))
+	fmt.Println(lrw.HashPassword(data, config.BCryptCost))
 }
